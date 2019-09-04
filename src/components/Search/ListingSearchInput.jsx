fd019634d4db6867f8f968710125d5b534bba06d
github username <github email address>
2019-09-03 22:28:09 -0700

    first commit

---
+++ src/components/Search/ListingSearchInput.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+export default class ListingSearchInput extends Component {
+  render() {
+    return (
+      <div className="listingsearchinput">
+        
+      </div>
+    )
+  }
+}
